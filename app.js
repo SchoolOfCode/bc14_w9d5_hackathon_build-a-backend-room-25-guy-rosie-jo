5362045420fa20958213683e631e032813d923c9
rosiegracejones <rosiegracejones@gmail.com>
2023-05-12 10:13:37 +0100

    started writing recipes.js CRUD functionality

--- app.js
+++ app.js
@@ -11,8 +11,23 @@ import {
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
+
+// all recipes
+app.get("/api/recipes", (res, req) => {
+  if (req.query.success === "true") {
+  res.json({})
+  res.send(getRecipes());
+} }
+)
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- recipes.js
+++ recipes.js
@@ -4,13 +4,34 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+    // read the json file
+    const recipeJSON = await fs.readFile(fileName, "utf-8");
+    // parse the json file
+    const recipes = JSON.parse(recipeJSON);
+    // return the recipes object
+    return recipes;
+}
 
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+export async function getRecipeByID(id) {
+    // read the json file
+    const recipeJSON = await fs.readFile(fileName, "utf-8");
+    // parse the json
+    const recipes = JSON.parse(recipeJSON);
+    // for loop
+    for (let i = 0; i < recipes.length; i++) {
+        if (recipes[i].id === id) {
+          let recipe = recipes[i];
+          return recipe;
+        }
+      }
+}
 
 // CREATE A RECIPE
-export async function createRecipe(newRecipe) {}
+export async function createRecipe(newRecipe) {
+    // uuidv4
+}
 
 // UPDATE A RECIPE BY ID
 export async function updateRecipeByID(id, updatedRecipe) {}
