c23a83d7d0c4fbf0955386284c8c1a3a2e5736c2
rosiegracejones <rosiegracejones@gmail.com>
2023-05-14 20:32:51 +0100

    all router & helper functions updated

--- app.js
+++ app.js
@@ -16,40 +16,51 @@ app.use(express.json());
 
 // Get all recipes
 app.get("/api/recipes", async (req, res) => {
-  const allRecipes = await getRecipes();
-  const success = allRecipes.length > 0;
-  //console.log(success);
-  res.send({success, payload: allRecipes});
-  });
+  const payload = await getRecipes();
+  const success = payload !== undefined;
+  if (success) {
+    res.json({ success, payload });
+  }
+  res.status(404).json({ success });
+});
 
 // Get recipe by ID
 app.get("/api/recipes/:id", async (req, res) => {
-  const recipeByID = await getRecipeByID(req.params.id);
-  const success = recipeByID !== null;
-  res.send({ success, payload: recipeByID });
-  //console.log(recipeByID);
+  const payload = await getRecipeByID(req.params.id);
+  const success = payload !== undefined;
+  if (success) {
+    res.send({ success, payload });
+  }
+  res.status(404).json({ success });
 });
 
 // Create new recipe
 app.post("/api/recipes", async (req, res) => {
-  const createdRecipe = await createRecipe(req.body); 
-  const success = createdRecipe !== null;
-  res.send({ success, payload: createdRecipe });
-  //console.log(createdRecipe);
+  const payload = await createRecipe(req.body);
+
+  const newRecipeBody =
+    payload.title &&
+    payload.ingredients &&
+    payload.instructions &&
+    payload.image;
+  const success = newRecipeBody !== undefined;
+  if (success) {
+    res.json({ success, payload: createdRecipe });
+  }
+  res.status(418).json({ success });
 });
 
 // Update recipe by ID
 app.patch("/api/recipes/:id", async (req, res) => {
-const updateRecipe = await updateRecipeByID(req.params.id, req.body);
-res.send({ success: true, payload: updateRecipe });
-//console.log(updateRecipe);
-})
+  const payload = await updateRecipeByID(req.params.id, req.body);
+  res.json({ success: true, payload });
+});
 
 // Delete recipe by id
 app.delete("api/recipes/:id", async (req, res) => {
-  const deletedRecipe = await deleteRecipeByID(req.params.id);
-
-})
+  const payload = await deleteRecipeByID(req.params.id);
+  res.json({success: true, payload})
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- recipes.js
+++ recipes.js
@@ -5,64 +5,56 @@ const fileName = "recipes.json";
 
 // GET ALL RECIPES
 export async function getRecipes() {
-    // read the json file
-    const recipeJSON = await fs.readFile(fileName, "utf-8");
-    // parse the json file
-    const recipes = JSON.parse(recipeJSON);
-    // return the recipes object
-    return recipes;
+  const data = await fs.readFile(fileName, "utf-8");
+  const recipes = JSON.parse(data);
+  return recipes;
 }
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {
-    const recipeJSON = await fs.readFile(fileName, "utf-8");
-    const recipes = JSON.parse(recipeJSON);
-
-    // for loop
-    for (let i = 0; i < recipes.length; i++) {
-        if (recipes[i].id === id) {
-          let recipe = recipes[i];
-          return recipe;
-        }
-      }
+  const data = await fs.readFile(fileName, "utf-8");
+  const recipes = JSON.parse(data);
+  for (let i = 0; i < recipes.length; i++) {
+    if (recipes[i].id === id) {
+      let recipe = recipes[i];
+      return recipe;
+    }
+  }
 }
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {
-    const recipeJSON = await fs.readFile(fileName, "utf-8");
-    // parse the json
-    const recipes = JSON.parse(recipeJSON);
-
-    const recipeAdded = {
-      id: uuidv4(),
-      title: newRecipe.title,
-      ingredients: newRecipe.ingredients,
-      instructions: newRecipe.instructions,
-      image: newRecipe.image
-    };
-
-  recipes.push(newRecipe);
-  await fs.writeFile(fileName, JSON.stringify(recipes));
-  return recipeAdded;
-  }
+  const recipes = await getRecipes();
+  const recipe = { ...newRecipe, id: uuidv4() };
+  recipes.push(recipe);
+  await fs.writeFile(fileName, JSON.stringify(recipes), "utf-8");
+  return recipe;
+}
 
 // UPDATE A RECIPE BY ID (not yet working)
 export async function updateRecipeByID(id, updatedRecipe) {
-  const recipeJSON = await fs.readFile(fileName, "utf-8");
-  const recipes = JSON.parse(recipeJSON);
-
-  const index = recipes.findIndex(recipe => recipe.id === id);
-  // console.log(index);
-  recipes.splice(index, 1, updatedRecipe);
-  // console.log(recipes);
-  return updatedRecipe;
-  // Feed in ID number 
-  // New object that feeds in to JSON 
-  // Replace recipe with that ID with a new updated recipe object 
+  const recipes = await getRecipes();
+  const index = recipes.findIndex((recipe) => recipe.id === id);
+  if (index >= 0) {
+    const preUpdate = recipes[index];
+    const updated = {
+      ...preUpdate,
+      ...updatedRecipe,
+      id: id,
+    };
+    recipes[index] = updated;
+    await fs.writeFile(fileName, JSON.stringify(recipes), "utf-8");
+    return recipes[index];
+  }
 }
 
 // DELETE A RECIPE BY ID
 export async function deleteRecipeByID(id) {
-  const recipeJSON = await fs.readFile(fileName, "utf-8");
-  const recipes = JSON.parse(recipeJSON);
+  const recipes = await getRecipes();
+  const index = recipes.findIndex((recipe) => recipe.id === id);
+    if (index >= 0) {
+      const [removedRecipe] = recipes.splice(index, 1);
+      await fs.writeFile(fileName, JSON.stringify(recipes), "utf-8");
+      return removedRecipe;
+    }
 }
